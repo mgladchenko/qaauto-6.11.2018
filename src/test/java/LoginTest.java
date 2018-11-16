852dadf05047c262f277d4e209da6a73e5315a9f
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-11-16 22:07:17 +0200

    Lesson 4

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,26 +3,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+    WebDriver webDriver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        webDriver = new ChromeDriver();
+        webDriver.get("https://www.linkedin.com");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        webDriver.quit();
+    }
 
     @Test
     public void negativeLoginTest() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.linkedin.com");
-
-        WebElement emailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
-        WebElement passwordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
-        WebElement signInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
-
-        emailField.sendKeys("a@b.c");
-        passwordField.sendKeys("");
-        signInButton.click();
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.login("a@b.c", "");
 
         //Verify that page title is "LinkedIn: Log In or Sign Up"
-        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign U");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign Up",
+                "Login page title is wrong.");
+        Assert.assertTrue(loginPage.signInButton.isDisplayed(), "SignIn button is not displayed.");
+    }
 
+    @Test
+    public void successfulLoginTest() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.login("linkedin.tst.yanina@gmail.com", "Test123!");
+
+        //Verify that page title is "LinkedIn: Log In or Sign Up"
+        WebElement welcomeMessage = webDriver.findElement(
+                By.xpath("//a[@data-control-name='identity_welcome_message']"));
+
+        Assert.assertTrue(webDriver.getTitle().contains("LinkedIn"),
+                "Home page title is wrong.");
+        Assert.assertTrue(welcomeMessage.isDisplayed(),
+                "Welcome message is not displayed is not displayed.");
 
 
     }
