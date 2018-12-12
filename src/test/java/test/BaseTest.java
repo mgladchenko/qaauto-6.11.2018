b317d6113ce4f7d5e80c10f7e2a4c7b48c1e69f0
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-12-12 11:42:59 +0200

    Home Task #11

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -6,16 +6,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import page.LoginPage;
 
 public class BaseTest {
     protected WebDriver webDriver;
     protected LoginPage loginPage;
 
+    @Parameters("browserName")
     @BeforeMethod
-    public void beforeMethod() {
-        WebDriverManager.firefoxdriver().setup();
-        webDriver = new FirefoxDriver();
+    public void beforeMethod(@Optional("firefox") String browserName) {
+        if (browserName.toLowerCase().equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }
+        if (browserName.toLowerCase().equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else {
+            try {
+                throw new Exception("browserName "+browserName+" is not supported.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         webDriver.get("https://www.linkedin.com");
         loginPage = new LoginPage(webDriver);
     }
